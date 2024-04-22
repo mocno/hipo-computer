026f96b8b58e167af8728d7056f0260d2b41dea2
mocno <gustavo.correa.mogi@gmail.com>
2024-04-22 16:16:19 +0000

    Put states in parent

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,46 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import { ControlUnit } from "./components/ControlUnit";
-import { Memory } from "./components/Memory";
-import { INSTRUCTIONS } from "./utils/instructions";
+import { Memory, WordMemory } from "./components/Memory";
+// import { INSTRUCTIONS } from "./utils/instructions";
+import { Output } from "./components/Output";
+import { Processador } from "./utils/parser";
 
-function InstructionsList() {
-  return (
-    <div className="instructions-list">
-      {INSTRUCTIONS.map(({ code, symbol, help }) => (
-        <div className="instruction-list">
-          <div>{code}</div>
-          <div>{symbol}</div>
-          <div>{help}</div>
-        </div>
-      ))}
-    </div>
-  );
-}
+// function InstructionsList() {
+//   return (
+//     <div className="instructions-list">
+//       {INSTRUCTIONS.map(({ code, symbol, help }) => (
+//         <div className="instruction-list">
+//           <div>{code}</div>
+//           <div>{symbol}</div>
+//           <div>{help}</div>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// }
 
 function App() {
   const [cursor, setCursor] = useState(0);
   const [running, setRunning] = useState(false);
+  const [values, setValues] = useState(Array.from({ length: 100 }, (_, k) => 1100 + k + 2));
+  const [accumulator, setAccumulator] = useState(0);
+  const [outputs, setOutputs] = useState("");
+
+  const process = new Processador(values, setValues, accumulator, setAccumulator, setCursor, printInOutput);
+  
+  function printInOutput(value: string|number) {
+    setOutputs(value.toString() + "\n" + outputs)
+  }
 
   function incrementCunsor() {
-    setCursor(cursor == 99 ? 0 : cursor + 1)
+    setCursor(cursor === 99 ? 0 : cursor + 1)
   }
 
   useEffect(() =>{
     if (running) {
+      process.parse(cursor);
+
       const id = setTimeout(incrementCunsor, 500);
 
       return () => clearTimeout(id);
@@ -37,13 +50,19 @@ function App() {
 
   return (
     <div className="App">
-      <Memory cursor={cursor} setCursor={setCursor} />
+      <Memory cursor={cursor} setCursor={setCursor} values={values} />
       <ControlUnit
         onStop={() => setRunning(false)}
         onRun={() => setRunning(true)}
         onNext={incrementCunsor}
         running={running}
       />
+      <WordMemory
+        value={accumulator}
+        position={"Acumulador"}
+        disableButtons
+      />
+      <Output value={outputs} />
     </div>
   );
 }
--- src/components/Memory.tsx
+++ src/components/Memory.tsx
@@ -1,58 +1,42 @@
 import { Dispatch, MouseEventHandler, SetStateAction, useState } from "react";
 import { MdIntegrationInstructions } from "react-icons/md";
 import { TiSortNumerically } from "react-icons/ti";
-import { INSTRUCTIONS_CODE_TO_SYMBOL } from "../utils/instructions";
+import { formatNumericRepresentation, tryInstructionRepresentation } from "../utils/representations";
 
 type MemoryProps = {
-  cursor: number,
-  setCursor: Dispatch<SetStateAction<number>>
+  values: number[];
+  cursor: number;
+  setCursor: Dispatch<SetStateAction<number>>;
 }
 
-export function Memory({cursor, setCursor}: MemoryProps) {
+export function Memory({ values, cursor, setCursor}: MemoryProps) {
   return (
     <div className="memory-ram-display">
-      {Array.from({ length: 100 }, (_, k) => (
-        <WordMemory
-          key={k}
-          position={k}
-          selected={k === cursor}
-          onClick={() => setCursor(k)}
-        />
-      ))}
+      {
+        values.map((value, k) => (
+          <WordMemory
+            value={value}
+            key={k}
+            position={k}
+            selected={k === cursor}
+            onClick={() => setCursor(k)}
+          />
+        ))
+      }
     </div>
   );
 }
 
 type WordMemoryParms = {
-  position: Number;
-  selected: Boolean;
-  onClick: MouseEventHandler<HTMLDivElement>;
+  value: number;
+  position: number|string;
+  selected?: boolean;
+  disableButtons?: boolean;
+  onClick?: MouseEventHandler<HTMLDivElement>;
 };
 
-function WordMemory({ position, selected, onClick }: WordMemoryParms) {
-  const [value, _setValue] = useState(
-    Math.floor(Math.random() * 9999 * 2 - 9999)
-  );
+export function WordMemory({ value, position, selected=false, disableButtons=false, onClick }: WordMemoryParms) {
   const [numRepr, setNumRepr] = useState(false);
-
-  function formatNumericRepresentation(value: number) {
-    return value >= 0
-      ? "+" + value.toString().padStart(4, "0")
-      : "-" + (-value).toString().padStart(4, "0")
-  }
-
-  function tryInstructionRepresentation(value: number) {
-    if (value < 0)
-      return false;
-
-    let instructionPart = Math.floor(value / 100) as (keyof typeof INSTRUCTIONS_CODE_TO_SYMBOL);
-    let addressPart = value % 100;
-
-    if (!(instructionPart in INSTRUCTIONS_CODE_TO_SYMBOL))
-      return false;
-  
-    return INSTRUCTIONS_CODE_TO_SYMBOL[instructionPart] + " " + addressPart;
-  }
 
   let instructionRepr = tryInstructionRepresentation(value);
 
@@ -60,27 +44,38 @@ function WordMemory({ position, selected, onClick }: WordMemoryParms) {
     setNumRepr(true);
   }
 
+  let representation;
+  if (numRepr)
+    representation = formatNumericRepresentation(value)
+  else if (instructionRepr === false)
+    setNumRepr(true);
+  else 
+    representation = instructionRepr;
+
   return (
     <div
       className={selected ? "memory-word memory-selected" : "memory-word"}
     >
       <div className="memory-word-header">
         <span className="memory-position">{position.toString()}</span>
-        <div className="memory-buttons">
-          <MdIntegrationInstructions size={15} color={
-            numRepr ? (instructionRepr === false ? "#e17784" : "#666666") : "#ffffff"
-          } />
-          <TiSortNumerically size={15} color={
-            numRepr ? "#ffffff" : "#666666"
-          }/>
-        </div>
+        {
+          disableButtons ? null :
+          <div className="memory-buttons">
+            <MdIntegrationInstructions size={15} color={
+              numRepr ? (instructionRepr === false ? "#e17784" : "#666666") : "#ffffff"
+            } onClick={() => numRepr ? setNumRepr(false) : null} />
+            <TiSortNumerically size={15} color={
+              numRepr ? "#ffffff" : "#666666"
+            } onClick={() => !numRepr ? setNumRepr(true) : null}/>
+          </div>
+        }
       </div>
       <div className="memory-word-value" onClick={onClick}>
-        <span>{
-          instructionRepr !== false ?
-          instructionRepr :
-          formatNumericRepresentation(value)
-        }</span>
+        {
+          numRepr ?
+          <input className="memory-word-input" maxLength={5} value={ representation as string }/> :
+          <span>{ representation }</span>
+        }
       </div>
     </div>
   );
