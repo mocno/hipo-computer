daaf51085be29e0f40f95a5ef4a069a58b7fa1d4
mocno <gustavo.corre.mogi@gmail.com>
2024-04-29 09:10:03 -0300

    Create disabled prop in ControlUnit

--- src/components/ControlUnit.tsx
+++ src/components/ControlUnit.tsx
@@ -1,18 +1,19 @@
 import { MouseEventHandler } from "react";
 
 type ControlUnitProps = {
-    running?: boolean;
-    onStop?: MouseEventHandler<HTMLButtonElement>;
-    onRun?: MouseEventHandler<HTMLButtonElement>;
-    onNext?: MouseEventHandler<HTMLButtonElement>;
+  running?: boolean;
+  onStop?: MouseEventHandler<HTMLButtonElement>;
+  onRun?: MouseEventHandler<HTMLButtonElement>;
+  onNext?: MouseEventHandler<HTMLButtonElement>;
+  disabled?: boolean;
 };
 
-export function ControlUnit({ running, onStop, onRun, onNext }: ControlUnitProps) {
-    return (
+export function ControlUnit({ running = false, onStop, onRun, onNext, disabled=false }: ControlUnitProps) {
+  return (
     <div className="control-unit">
-        <button className="control-button control-button-stop" disabled={!running} onClick={onStop}>Parar</button>
-        <button className="control-button control-button-run" disabled={running} onClick={onRun}>Iniciar</button>
-        <button className="control-button control-button-next" onClick={onNext}>Proximo</button>
+      <button className="control-button control-button-stop" disabled={!running || disabled} onClick={onStop}>Parar</button>
+      <button className="control-button control-button-run" disabled={running || disabled} onClick={onRun}>Iniciar</button>
+      <button className="control-button control-button-next" disabled={disabled} onClick={onNext}>Proximo</button>
     </div>
-    )
+  )
 }
